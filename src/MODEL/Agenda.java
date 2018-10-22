07da665b4e3768e11f652e61e2ecd73aaea09274
molpy1996 <victorbriere@yahoo.fr>
2018-10-22 22:01:51 +0200

    Implementation dans Rdv.java des getters/setters/constructeurs et instanciation de la classe Agenda.java + Debut d'implementation de void ajoutRdV(RdV _rdv)

--- src/MODEL/Agenda.java
+++ src/MODEL/Agenda.java
@@ -5,10 +5,47 @@
  */
 package MODEL;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author asus
  */
 public class Agenda {
     
+    ArrayList <RdV> agd;
+    
+    //getters
+    
+    //setters
+
+    //autres methodes
+    
+    /**
+     * sauvegarder ET fermer l'agenda
+     */
+    
+    /**
+     * afficher tous les Rdvs (par date et horaires croissants) entre 2 dates
+     */
+    
+    /**
+     * afficher les RdVs correspondant a un critere ou une conjonction de criteres
+     */
+    
+    /**
+     * modifier un RdV
+     */
+    
+    /**
+     * ajouter un RdV (si le creneau date-horaire est libre) (à faire dans agenda ?)
+     */
+    
+    /**
+     * supprimer un RdV (à faire dans Agenda ?)
+     */
+    
+    /**
+     * supprimer TOUS les RdVs (à faire dans agenda ?)
+     */
 }
--- src/MODEL/RdV.java
+++ src/MODEL/RdV.java
@@ -5,10 +5,80 @@
  */
 package MODEL;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  *
- * @author asus
+ * @author Victor BRIERE
  */
 public class RdV {
+    private LocalDate date;
+    private LocalTime h_debut;
+    private LocalTime h_fin;
+    private String libelle;
+    private boolean rappel;
+    
+    //constructeur par défaut
+    RdV(){
+        this.date=LocalDate.parse("0000-00-00");
+        this.h_debut=LocalTime.parse("00:00");
+        this.h_fin=LocalTime.parse("00:00");
+        this.libelle=null;
+        this.rappel=false;
+    }
+    
+    //constructeur surchargé
+    RdV(LocalDate _date, LocalTime _h_debut, LocalTime _h_fin, String _libelle, 
+            boolean _rappel){
+        
+    }
+    
+    //getters
+    LocalDate getDate(){
+        return this.date;
+    }
+    
+    LocalTime getHdebut(){
+        return this.h_debut;
+    }
+    
+    LocalTime getHfin(){
+        return this.h_fin;
+    }
+    
+    String getLibelle(){
+        return this.libelle;
+    }
+    
+    boolean getRappel(){
+        return this.rappel;
+    }
+    
+    //setters
+    
+    void setDate(LocalDate _date){
+        this.date=_date;
+    }
+    
+    void setHdebut(LocalTime _h_debut){
+        this.h_debut=_h_debut;
+    }
+    
+    void setHfin(LocalTime _h_fin){
+        this.h_fin=_h_fin;
+    }
+    
+    void setLibelle(String _libelle){
+        if(_libelle.length()<=30){
+        this.libelle=_libelle;
+        }
+    }
+    
+    void setRappel(boolean _rappel){
+        this.rappel=_rappel;
+    }
+    
+    //autres methodes
     
 }

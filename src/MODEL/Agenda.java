2e928b40c16afffc749d2ea32e02ed3df90bf804
molpy1996 <victorbriere@yahoo.fr>
2018-11-09 19:17:58 +0100

    débuggage ajoutRdV() et AfficherRdV(), finies d'être codées avec les 2 fonctions suppressionRdV() et suppressionRdV_all(), normalement elles sot correctes + ajout de FermerAgenda()

--- src/MODEL/Agenda.java
+++ src/MODEL/Agenda.java
@@ -7,114 +7,158 @@ package MODEL;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Formatter;
 
 /**
  *
- * @author asus
+ * @author Victor Brière
  */
 public class Agenda {
 
-    ArrayList<RdV> agd;
+    ArrayList<RdV> agd = null;
 
     /*
     String nom;
     
      */
     //constructeur par défaut
-    Agenda() {
+    public Agenda() {
         this.agd = null;
     }
 
     //constructeur surchargé
-    Agenda(ArrayList<RdV> _agd) {
+    public Agenda(ArrayList<RdV> _agd) {
+        this.agd = new <RdV> ArrayList();
         this.agd.addAll(_agd);
+    }
+
+    //getter
+    public ArrayList<RdV> getAgenda() {
+        return this.agd;
     }
 
     //autres methodes
     
     /**
-     * Ouvrir l'agenda d'une personne specifiee
-     * ==> si boolean rappel == true; afficher le libelle du RdV dans un pop-up 15min avant
+     * Ouvrir l'agenda d'une personne specifiee ==> si boolean rappel == true;
+     * afficher le libelle du RdV dans un pop-up 15min avant
      */
     
     
     /**
-     * sauvegarder ET fermer l'agenda
+     * sauvegarder l'agenda dans un fichier ET fermer l'agenda
      */
-    void FermerAgd(){
-        
-    }    
     
-    /**
-     * afficher tous les Rdvs (par date et horaires croissants) entre 2 dates
-     */
-    void afficherRdVs(LocalDate d1, LocalDate d2) {
-        ArrayList <RdV> plage_RdV = new ArrayList();
+    
+    public void FermerAgd() {
+        //on essaye d'ouvrir un fichier dans lequel enregistrer le contenu de l'agenda
+        try {
+            Formatter f_agenda = new Formatter("\\Projet_Agenda_ING3\\saved_agendas");
 
-        for (RdV elem_agenda : this.agd) {
-            //si le rdv inscrit dans l'agenda est compris entre d1 et d2
-            //d1<elem_agenda<d2 (OU d2<elem_agenda<d1 si d2<d1)
-            if ((elem_agenda.getDate().isAfter(d1) && elem_agenda.getDate().isBefore(d2))
-                    || (elem_agenda.getDate().isAfter(d2) && elem_agenda.getDate().isBefore(d1))) {
-                plage_RdV.add(elem_agenda);
+            for (RdV el : this.getAgenda()) {
+                //on enregistre 1 rdv par ligne
+                //faire gaffe aux formats : %..
+                f_agenda.format("%s/%s/%s de %s a %s : %s , %b",
+                        el.getDate().getDayOfMonth(), el.getDate().getMonthValue(),
+                        el.getDate().getYear(), el.getHdebut(),
+                        el.getHfin(), el.getLibelle(), el.getRappel());
+                //on supprime le rdv de l'arraylist d'agenda    
+                this.suppressionRdV(el);
             }
+            //on ferme le fichier en ecriture
+            f_agenda.close();
+            //on met l'attribut agenda à null une fois qu'il est "vidé"
+            this.agd = null;
+        } //en cas de probleme dans l'ouverture du fichier on affiche une erreur
+        catch (Exception e) {
+            System.out.print("Erreur d'ouverture du fichier\n");
         }
-        System.out.printf("Entre le ", d1.getDayOfMonth(), "/", d1.getMonthValue(), "/",
-                        d1.getYear(), " et le ", d2.getDayOfMonth(), "/", d2.getMonthValue(), "/",
-                        d2.getYear());
-        System.out.print("\n Vous avez les rdv suivants :\n");
-        
-        //TRIER plage_RdV par DATES et HORAIRES croissants !!!!!
-        
-        //on affiche la liste des rdv entre d1 et d2
-        for(RdV elem : plage_RdV){
-            System.out.println(elem.getLibelle());
-            System.out.printf("%d", elem.getDate().getDayOfMonth(), " / %d", 
-                    elem.getDate().getMonthValue(), " / %d", elem.getDate().getYear());
 
-        }
     }
 
     /**
-     * afficher les RdVs correspondant a un critere ou une conjonction de
-     * criteres
-     * ==> ici aussi il faut les afficher triés
+     * retourne tous les Rdvs (par date et horaires croissants) entre 2 dates
+     *
+     * @param d1
+     * @param d2
+     * @return
      */
-    
+    public ArrayList<RdV> afficherRdV(LocalDate d1, LocalDate d2) {
+        ArrayList<RdV> plage_RdV = new ArrayList();
+        if (this.getAgenda() != null) {
+            for (RdV elem_agenda : this.getAgenda()) {
+                //si le rdv inscrit dans l'agenda est compris entre d1 et d2
+                //d1<elem_agenda<d2 (OU d2<elem_agenda<d1 si d2<d1)
+                if ((elem_agenda.getDate().isAfter(d1) && elem_agenda.getDate().isBefore(d2))
+                        || (elem_agenda.getDate().isAfter(d2) && elem_agenda.getDate().isBefore(d1))) {
+                    plage_RdV.add(elem_agenda);
+                }
+            }
+            //on print la liste de rdv avant tri
+            System.out.print(plage_RdV);
+            //on print la liste de rdv avant tri
+            System.out.print("\n");
+            Collections.sort(plage_RdV);
+            System.out.print(plage_RdV);
+
+        }
+
+        return plage_RdV;
+
+    }
+
+    /**
+     * renvoyer une arrayList des RdVs correspondant a un critere ou une
+     * conjonction de criteres
+     *
+     */
     
     /**
-     * modifier un RdV (s'occcupe de l'affichage ET des modifs)
+     * modifier un RdV (s'occcupe pas de l'affichage)
      */
+    public void modifierRdV() {
+
+    }
     
-    
+
     /**
      * ajouter un RdV (si le creneau date-horaire est libre)
+     *
+     * @param _rdv
      */
-    void ajoutRdV(RdV _rdv) {
-        for (RdV elem_agenda : this.agd) {
+    public void ajoutRdV(RdV _rdv) {
+        for (RdV elem_agenda : this.getAgenda()) {
             //si le creneau est libre on ajoute le rdv dans l'agenda
-            if(!(_rdv.getDate().equals(elem_agenda.getDate())
+            if (!(_rdv.getDate().equals(elem_agenda.getDate())
                     && (_rdv.getHdebut().isAfter(elem_agenda.getHdebut()) && _rdv.getHdebut().isBefore(elem_agenda.getHfin())
                     || _rdv.getHfin().isAfter(elem_agenda.getHdebut()) && _rdv.getHfin().isBefore(elem_agenda.getHfin())))) {
-                this.agd.add(_rdv);
+                this.getAgenda().add(_rdv);
             }
         }
     }
 
+    
     /**
      * supprimer un RdV
+     *
+     * @param _rdv
      */
-    void suppressionRdV(RdV _rdv) {
-        this.agd.remove(_rdv);
+    public void suppressionRdV(RdV _rdv) {
+        //on supprime un rdv de l'agenda
+        this.getAgenda().remove(_rdv);
+        //on reinitialise a null l'objet rdv
+        _rdv = null;
     }
+    
 
     /**
      * supprimer TOUS les RdVs
      */
-    void suppressionRdV_all() {
+    public void suppressionRdV_all() {
         //on vide l'agenda de ses elements
         for (RdV elem_agenda : this.agd) {
-            this.agd.remove(elem_agenda);
+            this.getAgenda().remove(elem_agenda);
         }
         //on reinitialise a null l'objet agenda
         this.agd = null;
--- src/MODEL/RdV.java
+++ src/MODEL/RdV.java
@@ -12,101 +12,121 @@ import java.time.LocalTime;
  *
  * @author Victor BRIERE
  */
-public class RdV {
+public class RdV implements Comparable <RdV> {
+
     private LocalDate date;
     private LocalTime h_debut;
     private LocalTime h_fin;
     private String libelle;
     private boolean rappel;
-    
+
     //constructeur par défaut
-    RdV(){
-        this.date=LocalDate.parse("0000-00-00");
-        this.h_debut=LocalTime.parse("00:00");
-        this.h_fin=LocalTime.parse("00:00");
-        this.libelle=null;
-        this.rappel=false;
+    public RdV() {
+        this.date = LocalDate.parse("0000-00-00");
+        this.h_debut = LocalTime.parse("00:00");
+        this.h_fin = LocalTime.parse("00:00");
+        this.libelle = null;
+        this.rappel = false;
     }
-    
+
     //constructeur surchargé
-    RdV(LocalDate _date, LocalTime _h_debut, LocalTime _h_fin, String _libelle, 
-            boolean _rappel){
+    public RdV(LocalDate _date, LocalTime _h_debut, LocalTime _h_fin, String _libelle,
+            boolean _rappel) {
         this.setDate(_date);
         this.setHdebut(_h_debut);
         this.setHfin(_h_fin);
         this.setLibelle(_libelle);
         this.setRappel(_rappel);
     }
-    
+
     //getters
-    LocalDate getDate(){
+    public LocalDate getDate() {
         return this.date;
     }
-    
-    LocalTime getHdebut(){
+
+    public LocalTime getHdebut() {
         return this.h_debut;
     }
-    
-    LocalTime getHfin(){
+
+    public LocalTime getHfin() {
         return this.h_fin;
     }
-    
-    String getLibelle(){
+
+    public String getLibelle() {
         return this.libelle;
     }
-    
-    boolean getRappel(){
+
+    public boolean getRappel() {
         return this.rappel;
     }
-    
+
     //setters
-    
-    final void setDate(LocalDate _date){
-        this.date=_date;
+    public final void setDate(LocalDate _date) {
+        this.date = _date;
     }
-    
-    final void setHdebut(LocalTime _h_debut){
-        this.h_debut=_h_debut;
+
+    public final void setHdebut(LocalTime _h_debut) {
+        this.h_debut = _h_debut;
     }
-    
-    final void setHfin(LocalTime _h_fin){
-        this.h_fin=_h_fin;
+
+    public final void setHfin(LocalTime _h_fin) {
+        this.h_fin = _h_fin;
     }
-    
-    final void setLibelle(String _libelle){
-        if(_libelle.length()<=30){
-        this.libelle=_libelle;
+
+    public final void setLibelle(String _libelle) {
+        if (_libelle.length() <= 30) {
+            this.libelle = _libelle;
         }
     }
-    
-    final void setRappel(boolean _rappel){
-        this.rappel=_rappel;
-    }
-    
-    //autres methodes
 
+    public final void setRappel(boolean _rappel) {
+        this.rappel = _rappel;
+    }
+
+    //autres methodes
+    /**
+     * Comparaison de RdV en fonction de leur date et si dates égales en fct de
+     * leur horaire
+     *
+     * @param rdv1
+     * @param rdv2
+     * @return
+     */
     /**
      * afficher tous les Rdvs (par date et horaires croissants) entre 2 dates
      */
-    
     /**
-     * afficher les RdVs correspondant a un critere ou une conjonction de criteres
+     * afficher les RdVs correspondant a un critere ou une conjonction de
+     * criteres
      */
-    
     /**
      * modifier un RdV
      */
-    
     /**
-     * ajouter un RdV (si le creneau date-horaire est libre) (à faire dans agenda ?)
+     * ajouter un RdV (si le creneau date-horaire est libre) (à faire dans
+     * agenda ?)
      */
-    
     /**
      * supprimer un RdV (à faire dans Agenda ?)
      */
-    
     /**
      * supprimer TOUS les RdVs (à faire dans agenda ?)
      */
+
+    @Override
+    public int compareTo(RdV o) {
+        
+        int compare = this.getDate().compareTo(o.getDate());
+        if(compare == 0){
+            compare = this.getHdebut().compareTo(o.getHdebut());
+        }
+        return compare;       
+    }
     
+    @Override
+    public String toString(){
+        return "libelle : " + getLibelle()+  "date : " + getDate() + " Hdebut : " 
+                + getHdebut() + "\n";
+    }
+
 }
